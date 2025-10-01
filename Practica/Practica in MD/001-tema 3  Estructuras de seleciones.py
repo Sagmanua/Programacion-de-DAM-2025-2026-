9d6f8769b13801c929aa8f16f502efd60b717575
Sagmanua <163444640+Sagmanua@users.noreply.github.com>
2025-10-01 10:38:20 +0200

    Add files via upload

---
+++ Practica/Practica in MD/001-tema 3  Estructuras de seleciones.py
@@ -0,0 +1,43 @@
+'''
+    Clasifacacion de edades en ub VideoJuego
+    (c) Bohdan Sydorenko
+    Imagina que eres un desarrollador de videojuegos y necesitas clasificar a 
+    los jugadores según su edad para ofrecer contenido adecuado. Tienes 4 categorías principales: 
+    niño, adolescente, joven y no joven.
+
+
+'''
+
+'''
+Que hago
+1.creae Variable 
+2.Introduse datos 
+2.Utilaza conctructor  de control 
+3.Imprir la resulatado
+'''
+
+#Vaarible 
+edad = 0
+
+#Introduse tu edad 
+edad = int(input("Escribe tu edad entre  la 5 y 60 \n"))
+
+#Comprobar edad de usario 
+# Y Imprir  su edad depende de edad 
+
+if edad <=10 and edad>=5:
+    print("Eres un Nino")
+elif edad >= 10 and edad <=19:
+    print("Eres un adolescente")
+elif edad>= 20 and edad <29:
+    print("Eres un Joven")
+elif edad >=30 and edad <= 60:
+    print("No ya Eres no Joven")
+else:
+    print("Esribe la edad correcta por favor ")
+
+    
+        
+
+
+
