08c54c319e2daa370c43c195f7971c59e16985b6
Sagmanua <163444640+Sagmanua@users.noreply.github.com>
2025-10-07 12:01:52 +0200

    Add files via upload

---
+++ Practica/final ex/101 Ejercicio de final de unidad.py
@@ -0,0 +1,34 @@
+'''
+    Ejercicio de final de unidad
+    (c) Bohdan Sydorenko
+    En mi tiempo libre, disfruto mucho jugando videojuegos y viendo series anime. 
+    Esta actividad son una gran parte de mi diversión y me mantienen entretenido cuando no estoy ocupado con mis estudios.
+
+
+'''
+'''
+Que hago
+    1.variables
+    2.declarar varibles
+    3.calcula las goeas de video juegos
+    4.calculas las horas de episodios 
+    5.imprimir todo
+'''
+#declaracion variables
+tiempo_videojuegos = None
+episodios_vistos = None 
+
+#variavles 
+tiempo_videojuegos = 10
+episodios_vistos = 4 
+
+#calcula las goeas de video juegos
+horas_videojuegos_anules = tiempo_videojuegos * 365
+
+#calculas las horas de episodios 
+episodios_anules = episodios_vistos * 52
+
+#imprimir todo
+print("Horas de videojuegos al año:", horas_videojuegos_anules)
+print("Episodios de anime al año:", episodios_anules)
+
