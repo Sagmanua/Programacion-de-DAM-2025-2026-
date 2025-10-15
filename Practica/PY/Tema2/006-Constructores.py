a38172aa7b170e544f6047de5634a3eec16f0b8c
Sagmanua <163444640+Sagmanua@users.noreply.github.com>
2025-10-15 09:54:50 +0200

    Update 006-Constructores.py

--- Practica/PY/Tema2/006-Constructores.py
+++ Practica/PY/Tema2/006-Constructores.py
@@ -1,5 +1,5 @@
 '''
-    003-Utilazacion de metodos estaticos
+    006-COnstructores
     (c) Bohdan SYdorenko
     En nuestra actualidad digital, los videojuegos y las películas son dos de los hobbies más populares entre los jóvenes. Imagina un mundo donde estas actividades forman parte integral del día a día. 
     En este ejercicio, vamos a explorar cómo podemos utilizar objetos predeterminados en el lenguaje de programación para manejar estos hobbies.
@@ -10,4 +10,31 @@ Que hago
 
 '''
 
+import datetime
+
+Weekday=""
+
+fecha_actual = datetime.datetime.now()
+
+print("Hoy es el",fecha_actual.year,fecha_actual.month,fecha_actual.day )
+
+day = fecha_actual.weekday()
+
+if day == 0 :
+    Weekday = "lunes"
+elif day == 1 :
+    Weekday = "martes"
+elif day == 2 :
+    Weekday = "miercoles"
+elif day == 3 :
+    Weekday = "jueves"
+elif day == 4 :
+    Weekday = "viernes"
+elif day == 5 :
+    Weekday = "sábado"
+elif day == 6 :
+    Weekday = "domingo"
+    print("domingo")
+
+print("Día de la semana:",Weekday )
 
