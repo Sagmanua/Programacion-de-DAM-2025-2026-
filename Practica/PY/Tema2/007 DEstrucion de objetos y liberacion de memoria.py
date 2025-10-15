c7f0ebbb741fc2312cb4c271615dd9a611c24857
Sagmanua <163444640+Sagmanua@users.noreply.github.com>
2025-10-15 10:17:03 +0200

    Create 007 DEstrucion de objetos y liberacion de memoria.py

---
+++ Practica/PY/Tema2/007 DEstrucion de objetos y liberacion de memoria.py
@@ -0,0 +1,23 @@
+'''
+    006-COnstructores
+    (c) Bohdan SYdorenko
+    En esta actividad, vamos a utilizar objetos predefinidos en Python para crear una calculadora que determine el número de cuadras necesarias basándose en el número de caballos y cuántos caballos pueden estar en cada cuadra.
+
+
+'''
+'''
+
+
+'''
+
+#variables
+
+caballos = 0
+cuadra = 0
+
+caballos = int(input())
+
+cuadra = int(input())
+
+cuadra_nes = caballos/cuadra
+
