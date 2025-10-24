0ecde79d8d81a0d0995eb885005fa05ed808cbf7
Bohdan Sydorenko <163444640+Sagmanua@users.noreply.github.com>
2025-10-24 08:55:29 +0200

    1

--- Explication/Eval1/24.10/006 mysql.py
+++ Explication/Eval1/24.10/006 mysql.py
@@ -1,32 +1,23 @@
-import tkinter as tk 
+import mysql.connector
 
-def insertar():
-    print("!")
+conexion = mysql.connector.connect(
+    host="localhost",
+    user="empresadam",
+    password="Accesoadatos2526$",
+    database="empresadam"
+)
+cursor = conexion.cursor()
+cursor.execute('''
+  INSERT INTO clientes
+  VALUES(
+    1,
+    "Jose Vicente",
+    "Carratala Sanchis",
+    "info@jocarsa.com"
+  );
+''')
 
-ventana = tk.Tk()
-marco = tk.Frame(ventana)
-marco.pack(padx=20,pady=20)
+conexion.commit()
 
-#DNI/NIE
-tk.Label(marco,text="INtroduce el dni/nie del cliente").pack(padx=20,pady=20)
-dninie = tk.Entry()
-dninie.pack(padx=20,pady=20)
-
-#NOMBRE
-tk.Label(marco,text="INtroduce el nombre del cliente").pack(padx=20,pady=20)
-nombre = tk.Entry()
-nombre.pack(padx=20,pady=20)
-
-#APELLIDOS
-tk.Label(marco,text="INtroduce el apellidos del cliente").pack(padx=20,pady=20)
-apellidos = tk.Entry()
-apellidos.pack(padx=20,pady=20)
-
-#EMAIL
-tk.Label(marco,text="INtroduce el email del cliente").pack(padx=20,pady=20)
-email = tk.Entry()
-email.pack(padx=20,pady=20)
-
-tk.Button(marco,text="insertar",command=insertar).pack(padx=20,pady=20)
-
-ventana.mainloop()
+cursor.close()
+conexion.close()
