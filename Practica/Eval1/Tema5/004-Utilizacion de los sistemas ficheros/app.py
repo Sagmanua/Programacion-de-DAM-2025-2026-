ccec8d05b36cd805aea27d29d847b2ef06c8e9d0
Bohdan Sydorenko <163444640+Sagmanua@users.noreply.github.com>
2025-11-09 17:22:21 +0100

    Update app.py

--- Practica/Eval1/Tema5/004-Utilizacion de los sistemas ficheros/app.py
+++ Practica/Eval1/Tema5/004-Utilizacion de los sistemas ficheros/app.py
@@ -1,5 +1,6 @@
 import os
 
+# Parte 1
 carpeta = input("Indica una carpeta: ")
 
 elementos = os.listdir(carpeta)
@@ -7,3 +8,22 @@ elementos = os.listdir(carpeta)
 for elemento in elementos:
     ruta = os.path.join(carpeta, elemento)
     print(ruta)
+
+# Parte 2
+carpeta = input("Indica una carpeta: ")
+
+for directorio,carpetas,archivo in os.walk(carpeta):
+    print(directorio)
+    print(carpetas)
+    print(archivo)
+
+# Parte 3 
+archivo = open("mapa.txt",'r') # READ
+busca = input("Introduce el término a buscar: ")
+
+lineas = archivo.readlines()
+
+for linea in lineas:
+    if busca in linea:
+        print("###########################")
+        print("Encontrado!: ",linea)   
