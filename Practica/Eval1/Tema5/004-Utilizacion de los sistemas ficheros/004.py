d6918a889bc058be6dae15808834c3de9da990e6
Bohdan Sydorenko <163444640+Sagmanua@users.noreply.github.com>
2025-11-09 16:50:39 +0100

    Update 004.py

--- Practica/Eval1/Tema5/004-Utilizacion de los sistemas ficheros/004.py
+++ Practica/Eval1/Tema5/004-Utilizacion de los sistemas ficheros/004.py
@@ -4,29 +4,29 @@ carpeta = input("Ingresa la ruta de la carpeta: ")
 
 if os.path.isdir(carpeta):
     elementos = os.listdir(carpeta)
-    print("\nContenido de la carpeta:\n")
+    print("Contenido de la carpeta")
     for elemento in elementos:
         ruta_completa = os.path.join(carpeta, elemento)
         print(ruta_completa)
 
 
 
-carpeta = input("Ingresa la ruta de la carpeta: ")
+carpeta = input("Ingresa la ruta de la carpeta")
 
 if os.path.isdir(carpeta):
-    print("\nRecorriendo la carpeta y sus subcarpetas:\n")
+    print("Recorriendo la carpeta y sus subcarpetas:")
     for ruta_actual, carpetas, archivos in os.walk(carpeta):
-        print(f"Carpeta actual: {ruta_actual}")
+        print("Carpeta actual",ruta_actual)
         for nombre in archivos:
             ruta_completa = os.path.join(ruta_actual, nombre)
-            print(f"  Archivo: ",ruta_completa)
+            print("  Archivo: ",ruta_completa)
 
     archivo = input("Ingresa la ruta del archivo de texto: ")
     palabra = input("Ingresa la palabra a buscar: ")
 
 with open(archivo, "r", encoding="utf-8") as f:
-    print("\nResultado de la búsqueda:\n")
+    print("Resultado")
     for numero_linea, linea in enumerate(f, start=1):
         if palabra in linea:
-            print(f"Línea {numero_linea}: {linea.strip()}")
+            print(numero_linea)
 
