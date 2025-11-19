0d7fe15a0e760f3fd87882314597d25a39b54890
Sagmanua <163444640+Sagmanua@users.noreply.github.com>
2025-11-19 09:43:44 +0100

    1

---
+++ Explication/Arrays/Matrices arrays mulmesiodes/013 tipos.py
@@ -0,0 +1,7 @@
+lista = ['manzana','pernas','platonos']
+print(lista)
+print(type(lista))
+
+tupla = ['manzana','pernas','platonos']
+print(tupla)
+print(type(tupla))
---
+++ Explication/Arrays/Matrices arrays mulmesiodes/014 cambia tipos de datos.py
@@ -0,0 +1,9 @@
+tupla = ['manzana','pernas','platonos']
+print(tupla)
+# nesedito meter una fryta mas 
+lista = list(tupla) #convierto una tupla en la lista
+print(lista)
+lista.append("fresas")
+
+nueva_tupla = tuple(lista) #convierto la lista en una tupla
+print(nueva_tupla)
