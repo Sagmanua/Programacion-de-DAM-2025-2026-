7e36e6addb7f4c9da5e93c66d29f0ea36b6b22fd
Sagmanua <163444640+Sagmanua@users.noreply.github.com>
2025-11-19 09:24:52 +0100

    Create 010 agenda.py

---
+++ Explication/Arrays/Matrices arrays mulmesiodes/010 agenda.py
@@ -0,0 +1,10 @@
+agenda = []
+
+while True:
+    nombre = input("Dime tu nombre")
+    apellidos = input("Dime tu apellidos")
+    email = input("Dime tu email")
+    numeros = input("Dime tu munero")
+    #Añado a la agenda
+    agenda.append([nombre,apellidos,email,numeros])
+    print(agenda)
