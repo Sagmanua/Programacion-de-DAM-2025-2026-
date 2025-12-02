dca111a6f2d0f97089b8819f0780e7931b078579
Bohdan Sydorenko <163444640+Sagmanua@users.noreply.github.com>
2025-12-02 11:45:07 +0100

    PHP Ud-10 Programación en el lado del servidor

---
+++ Explication/PHP Ud-10 Programación en el lado del servidor/001 Fundamentos/012-estructura for y calendario.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<!doctype html>
+<html>
+	<head>
+        <style>
+        .div{border:1px solid black;padding:10px;width:50px;
+        height:50px;display:inline-block;}
+
+        </style>
+    </head>
+    <body>
+        <?php
+            for($dia = 1;$dia < 31;$dia++){
+                echo "<div class='div'>".$dia."<div>";
+            }
+        ?>
+    </body>
+</html>
