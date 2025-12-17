d377d667f254802256a625e1e5d8533c056a5d7e
Bohdan Sydorenko <163444640+Sagmanua@users.noreply.github.com>
2025-12-17 10:26:48 +0100

    006  mejoras estaticos

---
+++ Explication/PHP Ud-10 Programación en el lado del servidor/011 Supercontrolador/006  mejoras estaticos/back/index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+<head>
+    <link rel="stylesheet" href="css/estilo.css">
+
+</head>
+<body>
+    <?php include "inc/conexion_bd.php"; ?>
+
+    <nav>
+    	<?php include "controladores/poblar_menu.php" ?>
+    </nav>
+    <main>
+
+        <?php 
+        if(!isset($_GET['operacion'])){ // Si no hay operacion
+      		include "controladores/leer.php";
+        }else{
+        	if($_GET['operacion'] == "insertar"){
+          	include "controladores/insertar.php";
+          }
+          else if($_GET['operacion'] == "procesainsertar"){
+          	include "controladores/procesainsertar.php";
+        }
+        }   
+        
+         ?>
+    </main>
+</body>
+</html>
