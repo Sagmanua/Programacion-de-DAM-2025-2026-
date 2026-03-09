a93006491b8131eae28f3d85710af79c8cf3242f
= <163444640+Sagmanua@users.noreply.github.com>
2026-03-09 09:40:57 +0100

    011 Agenda IA

---
+++ Explication/012 inteligencia Artificial/011 Agenda IA/007-agente tonto.py
@@ -0,0 +1,3 @@
+archivo = open("/home/sagman/projects/Programacion-de-DAM-2025-2026-/Explication/012 inteligencia Artificial/011 Agenda IA/agente.txt",'a')
+archivo.write("Soy un agente, pero un poco tonto\n")
+archivo.close()
---
+++ Explication/012 inteligencia Artificial/011 Agenda IA/008-ahora creo cron.md
@@ -0,0 +1,3 @@
+crontan -e
+
+* * * * * /usr/bin/python3 "/home/sagman/projects/Programacion-de-DAM-2025-2026-/Explication/012 inteligencia Artificial/011 Agenda IA/programa_generado.py" 
