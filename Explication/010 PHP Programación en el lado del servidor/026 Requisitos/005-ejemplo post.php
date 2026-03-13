1b99cf1be6830523ddeb697c4195ab137a689ced
= <163444640+Sagmanua@users.noreply.github.com>
2026-03-13 09:35:10 +0100

    026 Requisitos

---
+++ Explication/010 PHP Programación en el lado del servidor/026 Requisitos/005-ejemplo post.php
@@ -0,0 +1,13 @@
+<form action="?" method="POST">
+    <input type="text" name="nombre">
+    <input type="text" name="apellidos">
+    <input type="submit">
+</form>
+
+<?php
+    if(isset($_POST['nombre'])){
+        foreach($_POST as $clave => $valor){
+            echo $clave . ": " . $valor . "<br>";
+        }
+    }
+?>
